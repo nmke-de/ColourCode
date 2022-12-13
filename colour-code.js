9ea5a1d30937cd85b6a2118a2438d37bfd7eff0c
nmke-de <github@nmke.de>
2022-12-14 00:02:11 +0100

    Added colour to URL

--- colour-code.js
+++ colour-code.js
@@ -1,6 +1,12 @@
 const input = document.getElementById("in");
 const body = document.getElementsByTagName("body")[0];
 
+window.onload = () => {
+	input.value = window.location.hash;
+	body.style.setProperty("background-color", window.location.hash);
+}
+
 input.oninput = () => {
 	body.style.setProperty("background-color", input.value);
+	window.location.hash = input.value;
 };
